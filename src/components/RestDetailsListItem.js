f8c485e6263e1a2db276f60c950458b93adb0783
Alper Karataş <alpkrts3@gmail.com>
2020-06-20 23:15:52 +0300

    Update RestDetailsListItem.js

--- src/components/RestDetailsListItem.js
+++ src/components/RestDetailsListItem.js
@@ -2,8 +2,6 @@ import React from 'react';
 import {View, Text} from 'react-native';
 
 const RestDetailsListItem = props => {
-  console.log('PROPS');
-  console.log(props);
   return (
     <View style={{justifyContent: 'center', alignItems: 'center'}}>
       <Text>{props.data.name}</Text>
